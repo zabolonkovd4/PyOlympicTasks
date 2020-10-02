5813db906bbaa785f785a9bcdc998a678256dc7c
Kuznetsov Mikhail <Freddy152@yandex.ru>
2020-10-02 17:27:05 +0300

    global var added

--- PyOlympicTasks/PyOlympicTasks.py
+++ PyOlympicTasks/PyOlympicTasks.py
@@ -1,6 +1,8 @@
 def search(cities, old_data, size, now, end, max):
     if (now == end):
         return size
+    global short_cut
+    if (size + 1) > short_cut: return -1
     old_data.append(now)
     results = []
     for i in cities[now]['neighbors']:
@@ -21,31 +23,32 @@ def search(cities, old_data, size, now, end, max):
         for i in results:
             if i == -1: continue
             if i < result: result = i
-        if result==max: return -1
-        else: return result
-
+        if result == max: return -1
+        else:
+           short_cut = result
+           return result
 ##################################################
-#############MAIN#################################
 cityNumbers = int(input())
 cities = []
 for i in range(cityNumbers):
     coord = input().split()
-    City={'x':int(coord[0]),
+    City = {'x':int(coord[0]),
          'y':int(coord[1]),
          'index':i,
         'neighbors':[]}
     cities.append(City)
 distance = int(input())
-distance= distance*distance
+distance = distance * distance
 first_last = input().split()
 first = int(first_last[0])
 last = int(first_last[1])
 
 for i in range(cityNumbers):
     for k in range(cityNumbers):
-        if i==k: continue
+        if i == k: continue
         R = (cities[i]['y'] - cities[k]['y']) * (cities[i]['y'] - cities[k]['y']) + (cities[i]['x'] - cities[k]['x']) * (cities[i]['x'] - cities[k]['x'])
         if R <= distance: cities[i]['neighbors'].append(k)
 old_data = []
-result = search(cities,old_data,0, first-1,last-1,cityNumbers)
-print(result)
+short_cut = cityNumbers
+result = search(cities,old_data,0, first - 1,last - 1,cityNumbers)
+print(short_cut)
